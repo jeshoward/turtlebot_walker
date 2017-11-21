287b8e50965c0e4d0f8b964789087a5fe0b600d0
Jessica H <jesmhoward@gmail.com>
2017-11-20 21:41:55 -0500

    Turtlebot now correctly avoids obstacles. He still escapes the edges of the map, though.

--- src/turtlebot_walker.cc
+++ src/turtlebot_walker.cc
@@ -42,9 +42,11 @@
 #include "tf/tf.h"
 #include "math.h"
 #include "ros/console.h"
-
-//Laser scan message to detect obstacles
+//Scan message to get distances to objects
 sensor_msgs::LaserScan scan_message_;
+
+//Twist message to change velocities
+geometry_msgs::Twist velocity_message_;
 
 //Publisher for velocity messages
 ros::Publisher velocity_publisher_;
@@ -57,52 +59,48 @@ ros::Subscriber pose_subscriber_;
 
 //Position and velocity messages
 nav_msgs::Odometry odometry_pose_;
-nav_msgs::Odometry obstacle_pose_;
 
 //Callback for the laser scan
-void scan_callback(sensor_msgs::LaserScan scan_message);
+void scan_callback(sensor_msgs::LaserScan::ConstPtr & scan_message);
 
 //Callback for the position and velocity message
 void pose_callback(const nav_msgs::Odometry::ConstPtr & pose_message);
 
-//Helper function to get distance between two points
-double get_distance(double x1, double y1, double x2, double y2);
-
-//Function to make the robot autonomously walk around the map
-void walk();
-
-//Function to avoid obstacles
-void avoid_obstacle();
-
-int main(int argc, char **argv) {
-	//Initiate our turtlebot_walker
-	ros::init(argc, argv, "turtlebot_walker");
-	ros::NodeHandle node_handle;
-	
-	//Subscribe to the laser scanner
-	scan_subscriber_ = node_handle.subscribe("/scan", 10, scan_callback);
-	
-	//Subscribe to the odometry to get the robot's position and velocity
-	pose_subscriber_ = node_handle.subscribe("odom", 10, pose_callback);
-	
-	//Register the velocity publisher
-	velocity_publisher_ = node_handle.advertise<geometry_msgs::Twist>
-		("/cmd_vel_mux/input/teleop", 1000);
-	
-	ros::spinOnce();
-	ros::Rate loop(1);
-	loop.sleep();
-	ros::spinOnce();
-	walk();
-	ros::spin();
-	return 0;
-}
 
 /* 
  * @brief Callback for the laser scan
  */
-void scan_callback(const sensor_msgs::LaserScan scan_message) {
-	scan_message_ = scan_message;
+void scan_callback(const sensor_msgs::LaserScan::ConstPtr & scan_message) {
+	scan_message_.ranges = scan_message->ranges;
+	
+	float min_range = scan_message_.ranges[0];
+	float max_range = scan_message_.ranges[0];
+	
+	for(float range : scan_message_.ranges) {
+		if(range < min_range && range > scan_message->range_min)
+			min_range = range;
+		if(range > max_range && range < scan_message->range_max)
+			max_range = range;
+	}
+
+	if(min_range < 0.6) {
+		//WE'RE GOING TO CRASH
+		ROS_INFO("Crash imminent. ABORT ABORT!");
+		velocity_message_.linear.x = -1;
+		velocity_message_.angular.x = 1;
+	} else if(min_range < 1) {
+		//OBSTACLE AHEAD, SPIN!
+		ROS_INFO("Encountered an obstacle. Spinning to avoid.");
+		velocity_message_.linear.x = 0;
+		velocity_message_.angular.z = 0.35;
+	} else {
+		ROS_INFO("Moving ahead.");
+		//Looks clear
+		velocity_message_.linear.x = 0.5;
+		velocity_message_.angular.z = 0;
+	}
+	
+	velocity_publisher_.publish(velocity_message_);
 }
 
 /* 
@@ -121,47 +119,31 @@ void pose_callback(const nav_msgs::Odometry::ConstPtr & pose_message) {
 	odometry_pose_.pose.pose.orientation.z = pose_message->pose.pose.orientation.z;
 }
 
-/* 
- * @brief Helper function to get distance between two points 
- * @param x1 x coordinate of first point
- * @param y1 y coordinate of first point
- * @param x2 x coordinate of second point
- * @param y2 y coordinate of second point
- * @return returns the distance between (x1,y1) and (x2, y2) 
- */
-double get_distance(double x1, double y1, double x2, double y2) {
-	return sqrt(pow((x1-x2),2) + pow((y1-y2),2));
+int main(int argc, char **argv) {
+	ROS_INFO("Initiating walker.");
+	//Initiate our turtlebot_walker
+	ros::init(argc, argv, "turtlebot_walker");
+	ros::NodeHandle node_handle;
+	
+	//Subscribe to the laser scanner
+	scan_subscriber_ = node_handle.subscribe("scan", 10, scan_callback);
+	
+	//Subscribe to the odometry to get the robot's position and velocity
+	pose_subscriber_ = node_handle.subscribe("odom", 10, pose_callback);
+	
+	//Register the velocity publisher
+	velocity_publisher_ = node_handle.advertise<geometry_msgs::Twist>
+		("/cmd_vel_mux/input/teleop", 1000);
+
+	//Set default velocity values
+	velocity_message_.linear.x = 0;
+	velocity_message_.linear.y = 0;
+	velocity_message_.linear.z = 0;
+	velocity_message_.angular.x = 0;
+	velocity_message_.angular.y = 0;
+	velocity_message_.angular.z = 0;
+	
+	ros::spin();
+	
+	return 0;
 }
-
-/* 
- * @brief Function to make the robot autonomously walk around the map 
- * @detail The turtlebot will move straight ahead until it encounters an obstacle,
- * when it gets close to the obstacle it will rotate until a clear path ahead is found
- * then it will continue to drive forward.
- */
-void walk() {
-	//generate a message to tell the robot where to go
-	geometry_msgs::Twist velocity_message;
-	
-	//set our robot at a specific starting point
-	
-	
-	ros::Rate loop_rate(100);
-	
-	bool obstacle = false;
-
-	do {
-		if(fmin(scan_message_.ranges) < 1.5) {
-			velocity_message.linear.x = 0;
-			velocity_message.angular.z = 0.35;
-		} else {
-			velocity_message.linear.x = 0.05;
-			velocity_message.angular.z = 0;
-		}
-
-		velocity_publisher_.publish(velocity_message);
-		ros::spinOnce();
-		
-	} while(ros::ok());
-}
-
